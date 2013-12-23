179a86debd8e206a3e3c1cda79ba94f14b98b721
Manfred Headley <manfred@razoredgelabs.com>
2013-12-23 17:42:50 -0500

    cleanup theme merge

---
+++ public_html/wp-content/themes/phil/footer.php
@@ -0,0 +1,17 @@
+      <footer>
+        <p>&copy; Wind&#333;<span class="brandcare">care</span> <?= date('Y');?></p>
+      </footer>
+    </div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="<?php bloginfo("template_url"); ?>js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+
+        <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/vendor/bootstrap.min.js"></script>
+
+        <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/main.js"></script>
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src='//www.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
---
+++ public_html/wp-content/themes/phil/index.php
@@ -0,0 +1,15 @@
+<?php include('header.php'); ?>
+
+		<div class="main">
+			<div class="container">
+				<div class="brandcontainer">  
+					<img src="http://placehold.it/100x60&text=Logo" alt="" class="brandlogo">
+					<h1 class="brand">Wind&#333;<span class="brandcare">care</span></h1>
+				</div>
+				<div class="mainslider">
+					<img src="http://placehold.it/1200x400&text=Sliding+Banner" alt="">
+				</div>
+			</div>
+		</div>
+
+<?php include('footer.php') ?>
