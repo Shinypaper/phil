b52f6b6ac59998aed65c8aa1b86594b4fabf4549
Manfred Headley <manfred@razoredgelabs.com>
2014-01-03 14:33:50 -0500

    changing the theme to dark and putting three calls inside main div

--- public_html/wp-content/themes/phil/page-home.php
+++ public_html/wp-content/themes/phil/page-home.php
@@ -7,26 +7,27 @@ get_header(); ?>
 	<section class="main homepage_main">
 		<div class="home_banner">
 			<figure>
-				<img src="http://www.philiphousenyc.com/wp-content/uploads/2013/10/RP-PHILIP-HOUSE-01-INT-PH-S-FINAL-2_optimized3.jpg" alt="">
+				<img src="<?php bloginfo('template_url'); ?>/assets/img/AURA75_LIVINGREZ.jpg" alt="">
 			</figure>
 			<div class="call_to_action">
 				<div class="wrapper">
 					<a href="#" class="action_link"><i class="fa fa-angle-double-right"></i></a>
 				</div>
-			</div>
-		</div>
-		<div class="wrapper">
-			
-			<section class="main_links">
+				<div class="wrapper">
 					
-				<div class="links_wrap">
-					<div class="link"><img src="http://placehold.it/300" alt=""></div>
-					<div class="link"><img src="http://placehold.it/300" alt=""></div>
-					<div class="link"><img src="http://placehold.it/300" alt=""></div>
+					<section class="main_links">
+							
+						<div class="links_wrap">
+							<div class="link"><img src="http://placehold.it/200x100&text=Buy" alt=""></div>
+							<div class="link"><img src="http://placehold.it/200x100&text=Sell" alt=""></div>
+							<div class="link"><img src="http://placehold.it/200x100&text=Contact" alt=""></div>
+
+						</div>
+
+					</section>
+
 				</div>
-
-			</section>
-
+			</div>
 		</div>
 	</section>
 
