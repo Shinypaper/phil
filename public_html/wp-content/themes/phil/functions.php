41f0eff09623a9cdefcc04eac53ad1472fae3247
dillon <dillon@razoredgelabs.com>
2014-01-07 19:18:34 -0500

    dillon setting up merge

--- public_html/wp-content/themes/phil/functions.php
+++ public_html/wp-content/themes/phil/functions.php
@@ -2,6 +2,25 @@
 
 add_theme_support( 'post-thumbnails' ); 
 
+function my_register_sidebars() {
+
+	/* Register the 'primary' sidebar. */
+	register_sidebar(
+		array(
+			'id' => 'blog-sidebar',
+			'name' => __( 'Blog Sidebar' ),
+			'description' => __( 'This will be displayed on the blog page' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3 class="widget-title">',
+			'after_title' => '</h3>'
+		)
+	);
+
+	/* Repeat register_sidebar() code for additional sidebars. */
+}
+add_action( 'widgets_init', 'my_register_sidebars' );
+
 function register_my_menus() {
   register_nav_menus(
     array(
--- public_html/wp-content/themes/phil/home.php
+++ public_html/wp-content/themes/phil/home.php
@@ -2,15 +2,19 @@
 
 		<div class="main">
 			<div class="wrapper">
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<article class="<?php post_class(); ?>">
-						<h2><?php the_title(); ?></h2>
-						<p><?php the_excerpt(); ?></p>
-					</article>
-				<?php endwhile; else: ?>
-				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-				<?php endif; ?>
+				<div class="wrapper_inner">
+					
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+						<article class="<?php post_class(); ?>">
+							<figure><?php the_post_thumbnail(); ?></figure>
+							<h2><?php the_title(); ?></h2>
+							<p><?php the_excerpt(); ?></p>
+						</article>
+					<?php endwhile; else: ?>
+					<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+					<?php endif; ?>
+					<?//php dynamic_sidebar( 'blog-sidebar' ); ?>
+				</div>
 			</div>
 		</div>
-
 <?php get_footer(); ?>
