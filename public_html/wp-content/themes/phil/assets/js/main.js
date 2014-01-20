3b857268d2c28d5bf5966e35af7ca82dfdd66ea6
dillon <dillon@razoredgelabs.com>
2014-01-19 22:46:47 -0500

    dillon bunch of templates and nav styles, also a new database dump

--- public_html/wp-content/themes/phil/assets/js/main.js
+++ public_html/wp-content/themes/phil/assets/js/main.js
@@ -1,18 +1,16 @@
 // $('.header').navobile({
-// 	cta: '.nav_toggle',
-// 	changeDOM: true
+//	cta: '.nav_toggle',
+//	changeDOM: true
 // });
 $('#header').navobile({
 	cta: '#show-navobile',
 	changeDOM: true
 });
-$('.menu .menu-item').each(function() {
-	console.log($(this).find('.sub-menu').length);
-	if ($(this).find('.sub-menu').length) {
-		$(this).children('a').append('<span class="caret"></span>');
-		$(this).click(function() {
-			$(this).siblings('.menu-item').find('.sub-menu').slideUp();
-			$(this).find('.sub-menu').slideToggle();
-		});
-	}
+$('.menu-item-has-children').each(function() {
+	$(this).children('a').append('<span class="caret"></span>');
+});
+$('.menu-item-has-children > a').click(function() {
+	$(this).parent().siblings('.menu-item').find('.sub-menu').slideUp();
+	$(this).siblings('.sub-menu').slideToggle();
+	return false;
 });
--- public_html/wp-content/themes/phil/home.php
+++ public_html/wp-content/themes/phil/home.php
@@ -13,14 +13,14 @@
 					<section class="main_blog">
 						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 							<article <?php post_class(); ?>>
-								<?php if (has_post_thumbnail()) { ?>
-									<figure><?php the_post_thumbnail(); ?></figure>
-								<? } ?>
 								<?php if (in_category('testimonials')) { ?>
 									<h3>Testimonial:</h3>
 								<? } ?>
 								<h3 class="post_title"><?php the_title(); ?></h3>
 								<small class="categories"><?php the_category(' | '); ?></small>
+								<?php if (has_post_thumbnail()) { ?>
+									<figure><?php the_post_thumbnail(); ?></figure>
+								<? } ?>
 								<p><?php the_excerpt(); ?></p>
 							</article>
 						<?php endwhile; else: ?>
