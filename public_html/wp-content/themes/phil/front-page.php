cbc0f843121e2fb55f249ef833ebb5bcd5953995
Manfred Headley <manfred@razoredgelabs.com>
2014-03-25 10:04:16 -0400

    team page stuff

--- public_html/wp-content/themes/phil/front-page.php
+++ public_html/wp-content/themes/phil/front-page.php
@@ -9,9 +9,9 @@
 				<section class="main_links">
 						
 					<div class="links_wrap">
-						<div class="link"><a href="<?php bloginfo('url') ?>/featured-listings"><img src="http://placehold.it/600&text=Buy" alt=""></a></div>
-						<div class="link"><a href="<?php bloginfo('url') ?>/sell"><img src="http://placehold.it/600&text=Sell" alt=""></a></div>
-						<div class="link"><a href="<?php bloginfo('url') ?>/neighbourhoods"><img src="http://placehold.it/600&text=Neighbourhoods" alt=""></a></div>
+						<div class="link"><a href="<?php bloginfo('url') ?>/featured-listings"><img src="<?php bloginfo('url')?>/wp-content/uploads/2014/01/Skyline_black_left.png" alt=""></a></div>
+						<div class="link"><a href="<?php bloginfo('url') ?>/sell"><img src="<?php bloginfo('url')?>/wp-content/uploads/2014/01/Skyline_black_centre.png" alt=""></a></div>
+						<div class="link"><a href="<?php bloginfo('url') ?>/neighbourhoods"><img src="<?php bloginfo('url')?>/wp-content/uploads/2014/01/Skyline_black_right.png" alt=""></a></div>
 					</div>
 
 				</section>
