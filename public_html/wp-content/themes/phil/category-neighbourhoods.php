900f7006290251437b65b9d8a687a70a967b3c9b
Manfred Headley <manfred@razoredgelabs.com>
2014-03-31 10:21:59 -0400

    merge

--- public_html/wp-content/themes/phil/category-neighbourhoods.php
+++ public_html/wp-content/themes/phil/category-neighbourhoods.php
@@ -4,13 +4,16 @@ get_header(); ?>
 
 		<div class="container">
 			<div class="main">
+			<img class="torontomap" src="<?=bloginfo('url')?>/wp-content/themes/phil//assets/img/torontomap.png" alt="">
 				<h2 class="page_title">Neighbourhoods</h2>
 				<div class="blogwrap">
 					<section class="neighbourhoods_posts main_blog">
 					
 						<?php $descendants = get_categories(array('child_of' => 6)); ?>
 						<?php foreach ($descendants as $child) { ?>
+						<div class="post">
 							<h1 class="post_title"><a href="<?= bloginfo('url'). "/category/neighbourhoods/" .$child->slug; ?>" class="neighbourhoods"> <?php echo $child->cat_name; ?></a></h1>
+						</div>
 							
 						<?php } ?>
 							
@@ -19,8 +22,6 @@ get_header(); ?>
 				</div>
 			</div>
 		</div>
-
-
 
 
 <?php get_footer(); ?>
